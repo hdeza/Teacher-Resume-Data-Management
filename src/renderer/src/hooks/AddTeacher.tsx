ca2d28f6c71dd04cb16a4146ef3ac11d70102978
hdeza <armandohdeza.03@gmail.com>
2024-07-31 21:56:47 -0500

    Primera pagina de añadir docente terminada

--- src/renderer/src/hooks/AddTeacher.tsx
+++ src/renderer/src/hooks/AddTeacher.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
-import DialogContent from '@mui/material/DialogContent'
-import DialogContentText from '@mui/material/DialogContentText'
-import DialogTitle from '@mui/material/DialogTitle'
 import AddIcon from '@mui/icons-material/Add'
-import { blue } from '@mui/material/colors'
+import CloseIcon from '@mui/icons-material/Close'
+import Pagination from '@mui/material/Pagination'
+import Stack from '@mui/material/Stack'
+
 export default function AddTeacher() {
   const [open, setOpen] = React.useState(false)
 
@@ -33,35 +31,69 @@ export default function AddTeacher() {
           component: 'form',
           onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
             event.preventDefault()
-            const formData = new FormData(event.currentTarget)
-            const formJson = Object.fromEntries((formData as any).entries())
-            const email = formJson.email
-            console.log(email)
             handleClose()
           }
         }}
       >
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
+        <section className="flex justify-between px-8 pt-7 ">
+          <article className="mr-40">
+            <h2 className="text-2xl font-bold">Añadir Docente</h2>
+            <p className="opacity-70">Ingrese los datos del nuevo docente</p>
+          </article>
+          <article>
+            <button className="opacity-60" onClick={handleClose}>
+              <CloseIcon />
+            </button>
+          </article>
+        </section>
+        <section className="px-8">
+          <article className="flex flex-col  pt-4 font-medium">
+            <label className="text-lg">Nombre</label>
+            <input
+              type="text"
+              placeholder="Escriba el nombre del docente"
+              className="border p-2 bg-gray-100 font-light rounded-md"
+            />
+          </article>
+          <article className="flex flex-col pt-4 font-medium">
+            <label className="text-lg">Apellido</label>
+            <input
+              type="text"
+              placeholder="Escriba el apellido del docente"
+              className="border p-2 bg-gray-100 font-light rounded-md"
+            />
+          </article>
+          <article className="flex flex-col pt-4 font-medium">
+            <label className="text-lg">N°Documento</label>
+            <input
+              type="number"
+              placeholder="Digite la cédula"
+              className="border p-2 bg-gray-100 font-light rounded-md"
+            />
+          </article>
+          <article className="flex flex-col pt-4 font-medium">
+            <label className="text-lg">Telefono</label>
+            <input
+              type="number"
+              placeholder="Digite número telefónico"
+              className="border p-2 bg-gray-100 font-light rounded-md"
+            />
+          </article>
+          <article className="flex flex-col pt-4 font-medium">
+            <label className="text-lg">Título</label>
+            <input
+              type="text"
+              placeholder="Escriba el titulo (pregrado)"
+              className="border p-2 bg-gray-100 font-light rounded-md"
+            />
+          </article>
+        </section>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Subscribe</Button>
+          <div className="py-4">
+            <Stack spacing={2}>
+              <Pagination count={3} shape="rounded" />
+            </Stack>
+          </div>
         </DialogActions>
       </Dialog>
     </>
